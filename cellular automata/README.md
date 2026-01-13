80d3aea4eebd79c4556b1c274e5577c3315e1c24
marineslimane <marineslimane05@gmail.com>
2026-01-13 19:20:30 +0100

    version finale

---
+++ cellular automata/README.md
@@ -0,0 +1,23 @@
+Description : 
+Visualisation de musiques grâce à un automate cellulaire qui se base sur le jeu de la vie. La génération des cellules se fait en fonction des fréquences (basses, moyennes) de la musique et des amplitudes.
+
+Indication :
+Les valeurs liées aux couleurs notamment dans fill(), et aux paramètres de la musique choisis pour la visualisation peuvent tous être modifiées dans le code pour obtenir des résultats bien diffèrents en fonction des préférences graphiques et des aspects de la musique que l'on souhaite mettre en avant. La taille des cellules peut également être modifiée...
+
+-> Pour changer de musique, importer la nouvelle musique sous format mp3 dans le dossier musics et changer le nom de fichier dans preload() dans music_visual.js
+
+Outils : 
+- VsCode
+- p5.js
+- p5.sound.js
+- HTML, CSS
+
+Remarques : 
+Je souhaitais, au début, utiliser l'API de Deezer pour faciliter le choix de la musique par l'utilisateur mais, ayant déjà réalisé un project avec cette API auparavant, j'ai fais fasse à un problème CORS c'est pourquoi j'ai abandonné l'idée.
+
+Crédits : 
+- p5.sound Reference : https://p5js.org/reference/p5.sound/
+- Game of Life : https://p5js.org/examples/math-and-physics-game-of-life/
+- p5 Reference : https://p5js.org/reference/
+- Random Circles with No Overlap, The Coding Train : https://share.google/dxb4wBPKrXi8Amoog
+- j'ai utilisé ChatGPT pour m'aider dans la conception des nouvelles règles
--- cellular automata/music_visual.js
+++ cellular automata/music_visual.js
@@ -9,25 +9,28 @@ let next_cells = [];
 let min_neighbours = 2; // nb minimum de voisins que doit avoir une cellule pour vivre
 let max_neighbours = 3; // nb maximum de voisins que doit avoir une cellule pour vivre
 let birth = 3; // nb de voisins nécessaires à une case pour qu'elle naisse
-let birthChance = 1.0; // probabilité qu'une naissance arrive
+let birth_chance = 1.0; // probabilité qu'une naissance arrive
+
+let canvas; // utile pour le max circulaire
 
 function preload() 
 {
     /* précharge la musique avant que setup() se lance */
 
-    song = loadSound('musics/TKS 2G - Whine (Clip officiel).mp3'); // changer la musique ICI
+    song = loadSound('musics/Sabrina Carpenter - Dont Smile.mp3'); // changer la musique ICI
 }
 
 function setup()
 {
-    /* prépare le terrain : récupère les informations sur la musique et intialise les tableaux et les valeurs de column_nb et row_nb */
+    /* prépare le terrain : récupère les informations sur la musique et initialise les tableaux et les valeurs de column_nb et row_nb */
 
     frameRate(20); // vitesse de draw() en fps (frames per second), attention à ne pas trop l'augmenter
-    createCanvas(400, 400);
+    canvas = createCanvas(400, 400);
+    canvas.parent("canvas"); // réfère à la div qui sert de mask pour l'affichage circulaire
     colorMode(HSB, 360, 100, 100, 100);
 
     // pour la musique :
-    fft = new p5.FFT(); // ftt contient des informations sont les fréquences : basses, moyennes et hautes (bass, mid, treble) qui pourront être récupérées avec des méthodes spécifiques
+    fft = new p5.FFT(); // ftt contient des informations sur les fréquences : basses, moyennes et hautes (bass, mid, treble) qui pourront être récupérées avec des méthodes spécifiques
     amp = new p5.Amplitude(); // amp contient des informations sur l'amplitude
 
     // calcul du nb de colonne et de lignes en fonction de la taille choisie pour les cellules
@@ -50,23 +53,24 @@ function setup()
 
 function draw()
 {
-    /**/
+    /* dessine le visuel (couleurs et forme des cellules) et fait le lien entre le visuel et le son en modifiant les valeurs globales qui servent dans les règles suivies par l'automate et décrites dans generate() */
 
-    background(0, 0, 0, 20);
+    background(250, 250, 250, 5); // fond blanc très transparent
 
     // pour la musique : 
     let level = amp.getLevel(); // volume global du son
     let bass = fft.getEnergy("bass"); // basses fréquences
-    let mid  = fft.getEnergy("mid"); // hautes fréquences
+    let mid  = fft.getEnergy("mid"); // moyennes fréquences : la voix plutot si voix dans la musique
+    // let treble  = fft.getEnergy("treble"); // hautes fréquences
 
     min_neighbours = floor(map(bass, 0, 255, 1, 3)); // bass & mid prenent des valeurs entre 0 et 255 et ici elles sont converties en un nombre de voisins pour avoir une représentation visuelle de l'évolution de la musique
     // si bass est nulle, le minimum de voisins pour rester en vie est 1 et si bass est forte (255) c'est 3 etc pour les valeurs intermédiaires  
     max_neighbours = floor(map(mid, 0, 255, 3, 8)); 
     // idem mais avec max. 8 pour le nombre maximum dans le cas de fortes fréquences moyennes car on prend en compte 8 voisins en tout pour une cellule : les 8 cases autour d'elle
     birth = floor(map(level, 0, 0.3, 2, 4)); // level prend des valeurs entre 0 et 0.3, idem pour la conversion. ici, elle détermine la naissance
-    birthChance = map(level, 0, 0.3, 0.2, 1.0); 
+    birth_chance = map(level, 0, 0.3, 0.2, 1.0); 
 
-    generate();
+    generate(); // génération des cellules
 
     // pour chaque cellule : 
     for (let column = 0; column < column_nb; column++)
@@ -76,35 +80,60 @@ function draw()
             let cell = current_cells[column][row];
 
             // coloration de la cellule : 
-            let c = map(cell, 0, 1, 0, 255); // conversion de la valeur de la cellule (0 ou 1) en couleur (noir ou blanc)
-            let hue = map(bass, 0, 255, 180, 360);
-            fill(hue, 80, 100, cell * 30);
-            noStroke(); // pas de bordure
+            let hue = map(mid, 0, 255, 180, 360); // selon l'intensité des fréquences mid, la hue va aller des tons plutot bleu à des tons plutot rouge
+            fill(hue, 100, 100, cell * 40); // saturation & luminosité maximale
+            // pour alpha, si cell = 0 ie cellule morte, la cellule est invisible et si cell = 1 ie cellule vivante, elle est visible avec une certaine transparence
+            // remarque : en fonction du choix de la variable de map() dans hue, mid ou bien bass ou bien treble ou highMid.. on obtient des résultats visuels différents car on se concentre sur différents aspects de la musique
+            // les autres paramètres de fill et l'intervalle choisi de couleur dans hue sont bien sûr aussi modifiables pour obtenir d'autres rendus
+            noStroke(); // pas de bordure ie grille invisible
 
+           
             // forme de la cellule : 
-            rect(column * cell_size, row * cell_size, cell_size, cell_size);
+            if (cell === 1)  // seulement si la cellule est vivante
+            {
+                let x = column * cell_size // position x de la cellule
+                let y = row * cell_size // position y de la cellule
+                let r = cell_size * random(0.5, 1.5);  // rayon choisie pour le cercle : random permet d'avoir de l'aléatoire sur le rayon pour un effet plus vivant et moins répétitif
+                
+                circle(x, y, r);
+            }
         }
     }
 }
 
 function mousePressed()
 {
-    /* lance la musique et la génération seulement une fois la souris pressée et randomise le visuel à chaque fois que l'on clique sur l'écran */
+    /* arrête la musique et la génération quand la souris est cliquée puis relance tout quand elle est recliquée */
 
-    userStartAudio(); // la musique joue
+    userStartAudio(); // au départ, la musique joue seulement quand l'écran est cliqué
 
-    if (!song.isPlaying()) 
+    if (song.isPlaying())
     {
-        song.loop();
+        song.pause(); // quand l'écran est recliqué, la musique se met en pause
+        noLoop(); // et l'animation aussi
     }
+    else
+    {
+        song.loop(); // sinon la musique joue en boucle
+        randomizeBoard(); // quand on relance ça randomise le visuel
+        loop(); // et le visuel se relance
+    }
+}
 
-    randomizeBoard();
-    loop();
+function toggleMusic() 
+{
+  if (song.isPlaying()) {
+    song.pause();
+    noLoop(); // stop l'animation
+  } else {
+    song.loop();
+    loop(); // relance l'animation
+  }
 }
 
 function randomizeBoard()
 {
-    /* randomise le visuel */
+    /* randomise le visuel pour avoir un autre 'pattern' */
 
     for (let column = 0; column < column_nb; column++) 
     {
@@ -149,9 +178,9 @@ function generate()
             {
                 next_cells[column][row] = 0; // elle meurt 
             }
-            else if (!alive && neighbours === birth && random() < birthChance) // si la cellule est morte et elle a exactement birth voisins et une probabilité non nulle de naissance
+            else if (!alive && neighbours === birth && random() < birth_chance) // si la cellule est morte et elle a exactement birth voisins et une probabilité non nulle de naissance
             {
-                next_cells[column][row] = 1;
+                next_cells[column][row] = 1; // elle vit
             }
             else // sinon
             {
